2c1eaa695ce28dea85dc9867b915f13e07849085
MateusCotaLima <mateuscotali@gmail.com>
2024-09-02 21:34:33 -0300

    Itabira (da ideia ao sucesso)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import './styles/variables.css';
 import Footer from './components/footer/Footer';
+import Itabira from './components/itabira/Itabira';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>  
     {/* <Navbar /> */}
+    <Itabira/>
     <Footer/>
   </React.StrictMode>
 );
