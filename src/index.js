c864a7fb2b039dd17f98bb389beb8c8e88064a73
MateusCotaLima <72894037+MateusCotaLima@users.noreply.github.com>
2024-09-05 15:08:46 -0300

    Criacao Component Missao Visão Valor

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './styles/global.css';
 import './styles/variables.css';
 import Footer from './components/footer/Footer';
 import Itabira from './components/itabira/Itabira';
+import Missao from './components/missao/Missao';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>  
     {/* <Navbar /> */}
+    <Missao/>
     <Itabira/>
     <Footer/>
   </React.StrictMode>
