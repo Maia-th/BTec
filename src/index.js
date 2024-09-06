973549e4e85a3dd057cf6ba1517059033f629f27
Maia.th <thiagomaia633@gmail.com>
2024-09-06 16:37:57 -0300

    feat: Component Parceiros

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import NavBar from './components/navbar-institucional/NavbarInstitucional';
 import HeaderBtec from './components/header-btec/HeaderBtec';
 import './styles/global.css';
 import './styles/variables.css';
+import Parceiros from './components/parceiros/Parceiros';
 import Footer from './components/footer/Footer';
 import Itabira from './components/itabira/Itabira';
 
@@ -13,6 +14,7 @@ root.render(
     <NavBar /> 
     <HeaderBtec/>
     <Itabira/>
+    <Parceiros/>
     <Footer/>
   </React.StrictMode>
 );
