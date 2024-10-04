0fe8894b93552ace2abf3ce4bedff6c84783cd00
Maia.th <thiagomaia633@gmail.com>
2024-10-04 00:08:24 -0300

    feat: component galeria

--- src/pages/startup/Startup.jsx
+++ src/pages/startup/Startup.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../../components/navbar-institucional/NavbarInstitucional";
 import HeaderStartup from "../../components/startup/header-startup/HeaderStartup";
 import SectionStartup from "../../components/startup/sectionStartup/SectionStartup";
+import Galeria from "../../components/startup/galeria/Galeria";
 import Footer from "../../components/footer/Footer";
 
 export default function Startup() {
@@ -10,6 +11,7 @@ export default function Startup() {
       <NavBar />
       <HeaderStartup />
       <SectionStartup />
+      <Galeria />
       <Footer />
     </React.StrictMode>
   );
