7296e1d7a26d400a6d3d2899e7354b11022146ad
MateusCotaLima <mateuscotali@gmail.com>
2024-11-04 11:52:57 -0300

    inicio arrumar links

--- src/components/btec/programas/Programas.jsx
+++ src/components/btec/programas/Programas.jsx
@@ -12,7 +12,7 @@ function programas (){
                 <div className="info">
                     <h3>Programa de Incubação</h3>
                     <p>O PIBTec (Programa de Incubação da BTec) é o coração da BTec, o programa tem o objetivo de auxiliar o desenvolvimento de novos negócios de base tecnológica (não escaláveis e escaláveis) por meio da jornada do empreendedor, com foco na geração de resultados inovadores e atendimento das demandas locais. A jornada que o empreendedor irá percorrer está dividida em três grandes etapas e tem duração entre um a três anos.</p>
-                    <button>Inscreva-se no PIBTec</button>
+                    <button onClick={() => window.location.href = '/PIBTec'}>Inscreva-se no PIBTec</button>
                 </div>
             </article>
 
@@ -20,7 +20,7 @@ function programas (){
                 <div className="info">
                     <h3>Programa de Mentoria</h3>
                     <p>O PMBTec (Programa de Mentoria da BTec) corresponde a um processo de desenvolvimento profissional para empreendedores que necessitam de um acompanhamento em seu negócio, com base em planejamentos pré-estabelecidos ou ações situacionais, orientado por um mentor profissional. </p>
-                    <button>Inscreva-se no PMBTec</button>
+                    <button onClick={() => window.location.href = '/PMBTec'}>Inscreva-se no PMBTec</button>
                 </div>
                 <img src={mentoria} alt="Livros" />
             </article>
